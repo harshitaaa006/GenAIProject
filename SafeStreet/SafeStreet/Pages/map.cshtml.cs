02943bbb7ae32de61e72118183acfc20419adade
Amy Chen <chen4cg@mail.uc.edu>
2024-11-13 12:36:22 -0500

    add mapbox api in the map

---
+++ SafeStreet/SafeStreet/Pages/map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafeStreet.Pages
+{
+    public class mapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
