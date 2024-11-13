46909990617dc1f647b8be541ca95c8825e9bcf0
Harshita Patel <patel4hr@mail.uc.edu>
2024-11-13 13:44:26 -0500

    Made the About us page and updated the privacy policy page

---
+++ SafeStreet/SafeStreet/Pages/CustomPages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafeStreet.Pages.CustomPages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
