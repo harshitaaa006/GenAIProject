4705fa30363426ed7c02f7d4402b3947e87be1a3
Harshita Patel <patel4hr@mail.uc.edu>
2024-11-13 14:50:17 -0500

    Added Cross-Page Navigation for Improved Accessibility

---
+++ SafeStreet/SafeStreet/Pages/CustomPages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafeStreet.Pages.CustomPages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
