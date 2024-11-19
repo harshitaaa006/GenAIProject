0bb39a32806216d5453f1a969d944359ddba9580
Adenike_driod <adenirae@mail.uc.edu>
2024-11-19 10:15:57 -0800

    After reviewing i made chages to the Search.cshtml, Search.cshtml.cs, and _Layout.cshtml pages

--- SafeStreet/SafeStreet/Pages/Search.cshtml.cs
+++ SafeStreet/SafeStreet/Pages/Search.cshtml.cs
@@ -1,12 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SafeStreet.Pages
 {
     public class SearchModel : PageModel
     {
-        public void OnGet()
-        {
-            
-        }
+        //I Updated the model (SearchModel) to include a property for the query string, This binds the input value directly to the Query property, reducing manual handling.
+        [BindProperty(SupportsGet = true)]
+        public string Query { get; set; }
     }
 }
